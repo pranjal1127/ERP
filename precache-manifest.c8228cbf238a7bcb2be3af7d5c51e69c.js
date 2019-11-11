e9b0d9255444683feb43069b144b1b2b7ee9f3c5
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2019-11-12 02:15:33 +0530

    Updates

---
+++ precache-manifest.c8228cbf238a7bcb2be3af7d5c51e69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e40d530578377e2941b22fcea81b19",
+    "url": "/erp/index.html"
+  },
+  {
+    "revision": "37c634ee029eb26d4189",
+    "url": "/erp/static/css/main.6a76abca.chunk.css"
+  },
+  {
+    "revision": "9b479b22b1dfd50419d7",
+    "url": "/erp/static/js/2.0c5d961b.chunk.js"
+  },
+  {
+    "revision": "37c634ee029eb26d4189",
+    "url": "/erp/static/js/main.49ba80ac.chunk.js"
+  },
+  {
+    "revision": "99de89205839825d187b",
+    "url": "/erp/static/js/runtime-main.64717216.js"
+  },
+  {
+    "revision": "08358d9ab2f580d197250832aeeafece",
+    "url": "/erp/static/media/back.08358d9a.jpg"
+  }
+]);
